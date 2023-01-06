39c8a669370da97e65d8df072ba2f907a447e80d
Thomas Wu <62056970+thomaswcy@users.noreply.github.com>
2023-01-06 09:26:07 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -69,6 +69,54 @@ const DocsPage = () => (
     <Container>
       <Heading variant="headline">Containers</Heading>
     </Container>
+ <Grid gap={3} columns={[null, 2]} as="form" variant="cards.sunken">
+          <Label>
+            Full name
+            <Input placeholder="Zach Latta" />
+          </Label>
+          <Label>
+            How are you primarily associated with Hack Club?
+            <Select>
+              <option value="" disabled hidden>
+                Select one…
+              </option>
+              <option value="club-leader">I lead a club</option>
+              <option value="club-member">I am a club member</option>
+              <option value="slack-member">I am active on Slack</option>
+              <option value="alum">I am a Hack Club alum</option>
+              <option value="none">None of the above</option>
+            </Select>
+          </Label>
+          <Label variant="labelHoriz">
+            <Checkbox />
+            Remember me
+          </Label>
+          <Flex sx={{ flexWrap: 'wrap' }}>
+            <Label variant="labelHoriz">
+              <Radio name="letter" /> Alpha
+            </Label>
+            <Label variant="labelHoriz">
+              <Radio name="letter" /> Bravo
+            </Label>
+            <Label variant="labelHoriz">
+              <Radio name="letter" /> Charlie
+            </Label>
+          </Flex>
+          <Label>
+            Why do you want to come?
+            <Textarea placeholder="Write a few sentences." />
+          </Label>
+          <Label>
+            Slider
+            <Slider color="red" />
+          </Label>
+          <Button
+            as="button"
+            type="submit"
+            children="RSVP"
+            sx={{ gridColumn: [null, 'span 2'] }}
+          />
+        </Grid>
   </>
 )
 
