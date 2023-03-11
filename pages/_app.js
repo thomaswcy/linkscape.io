4d04ed6efa53dd695e03847972e7a54f5d258138
Thomas Wu <62056970+thomaswcy@users.noreply.github.com>
2023-03-11 22:01:17 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,14 @@ import NextApp from 'next/app'
 import '@hackclub/theme/fonts/reg-bold.css'
 import theme from '@hackclub/theme'
 import { ThemeProvider } from 'theme-ui'
+import Nav from '../components/nav'
 
 export default class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props
     return (
       <ThemeProvider theme={theme}>
+        <Nav />
         <Component {...pageProps} />
       </ThemeProvider>
     )
