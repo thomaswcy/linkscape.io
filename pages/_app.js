97ad5eec3e887a67224c38b9c1a55fa849cb3f54
Thomas Wu <62056970+thomaswcy@users.noreply.github.com>
2023-03-11 21:49:56 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import NextApp from 'next/app'
 import '@hackclub/theme/fonts/reg-bold.css'
 import theme from '@hackclub/theme'
 import { ThemeProvider } from 'theme-ui'
-import ColorSwitcher from '../components/color-switcher'
 
 export default class App extends NextApp {
   render() {
